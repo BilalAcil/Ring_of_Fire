191ad72f485ad5c2f57c0162e711f6bc3f306bed
BilalAcil <bilal-acil@web.de>
2026-02-10 09:32:12 +0100

    Kartenkarussell

--- ringoffire/src/app/start-screen/start-screen.ts
+++ ringoffire/src/app/start-screen/start-screen.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-start-screen',
+  standalone: true,
   imports: [],
   templateUrl: './start-screen.html',
   styleUrl: './start-screen.scss',
 })
 export class StartScreen {
+  @ViewChild('wheelEl', { static: true }) wheelEl!: ElementRef<HTMLImageElement>;
 
+  @HostListener('window:scroll')
+  onScroll() {
+    const y = window.scrollY;
+    const deg = -(y / 5);
+
+    // Nur noch die Rotation setzen, kein translateX mehr nötig
+    this.wheelEl.nativeElement.style.transform = `rotate(${deg}deg)`;
+  }
 }
