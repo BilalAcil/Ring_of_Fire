79eed1127c993473177d05b07b328a7b1b8806b5
BilalAcil <bilal-acil@web.de>
2026-02-11 09:16:56 +0100

    button

--- ringoffire/src/app/start-screen/start-screen.ts
+++ ringoffire/src/app/start-screen/start-screen.ts
@@ -18,4 +18,21 @@ export class StartScreen {
     // Nur noch die Rotation setzen, kein translateX mehr nötig
     this.wheelEl.nativeElement.style.transform = `rotate(${deg}deg)`;
   }
+
+  isMoving = false;
+  private moveTimer: any;
+
+  onMove(e: MouseEvent) {
+    const el = e.currentTarget as HTMLElement;
+    el.style.setProperty('--mx', `${e.clientX}px`);
+    el.style.setProperty('--my', `${e.clientY}px`);
+
+    this.isMoving = true;
+
+    clearTimeout(this.moveTimer);
+    this.moveTimer = setTimeout(() => {
+      this.isMoving = false;
+    }, 2000);
+  }
+
 }
