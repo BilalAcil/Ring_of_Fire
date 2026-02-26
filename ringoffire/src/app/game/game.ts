6289914f0d234ae5503f6545f785104fe452a175
BilalAcil <bilal-acil@web.de>
2026-02-26 06:16:08 +0100

    dialog open

--- ringoffire/src/app/game/game.ts
+++ ringoffire/src/app/game/game.ts
@@ -1,19 +1,34 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameModel } from '../../models/gameModel';
 import { Player } from '../player/player';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayer } from '../dialog-add-player/dialog-add-player';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, Player],
+  imports: [CommonModule, Player, MatButtonModule, MatIconModule, MatDialogModule],
   templateUrl: './game.html',
   styleUrls: ['./game.scss'],
 })
-export class Game {
+export class Game implements OnInit {
   game = new GameModel();
   currentCard = '';
   isAnimating = false;
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new GameModel();
+  }
 
   @ViewChild('pickedCard') pickedCardRef?: ElementRef<HTMLImageElement>;
 
@@ -44,5 +59,14 @@ export class Game {
         this.isAnimating = false;
       }, 1000); // muss zu CSS passen
     }, 0);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayer);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    }
+    );
   }
 }
